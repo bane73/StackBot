6aea77f54ad0bae675bbd9a76d314fea5e6d3a3a
brandon <brandon@thegreshams.net>
2018-05-15 09:52:27 -0400

    Can get my comments, WITH the body!

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -5,6 +5,9 @@ import com.google.code.stackexchange.common.PagedList;
 
 import com.google.code.stackexchange.client.StackExchangeApiClient;
 import com.google.code.stackexchange.client.StackExchangeApiClientFactory;
+import com.google.code.stackexchange.client.query.CommentApiQuery;
+import com.google.code.stackexchange.client.query.impl.CommentApiQueryImpl;
+import com.google.code.stackexchange.client.query.StackExchangeApiQueryFactory;
 import com.google.code.stackexchange.schema.Comment;
 import com.google.code.stackexchange.schema.StackExchangeSite;
 
@@ -19,25 +22,39 @@ public class Runner {
     
     String keyValue = "PWLEN8DkcLJAbclSsjz1cw((";
     String siteValue = "stackoverflow";
-    Long idValue = 483257L;
+    String accessToken = "be0PQ3cRESzh35NlAUoFCw))";
     
-    StackExchangeApiClientFactory factory = StackExchangeApiClientFactory.newInstance(keyValue, StackExchangeSite.fromValue(siteValue));
+    Long idValue = 483257L; 
+    
+    StackExchangeApiClientFactory factory = StackExchangeApiClientFactory.newInstance(keyValue, StackExchangeSite.STACK_OVERFLOW);
     StackExchangeApiClient client = factory.createStackExchangeApiClient();
     
-    List<Long> userCommentIds = new ArrayList<>();
-    List<Comment> userComments = client.getUsersComments(Long.valueOf(idValue));
-    for (Comment c : userComments) {
-      userCommentIds.add(c.getCommentId());
-      //Comment c = client.getComments(uc.getCommentId()).get(0);
-      //System.out.println(">" + c.getOwner().getDisplayName() + ":" + c.getBody());
-      //System.out.println(uc.getOwner().getDisplayName() + ":" + comment.getCommentId());
+    System.out.println("UsersComments:");
+    for (Comment c : client.getUsersComments(Long.valueOf(idValue)) ) {      
+      String s = "";
+      s += "ID: " + c.getCommentId(); 
+      s += " ";
+      s += "OWNER: " + c.getOwner();
+      s += " ";
+      s += "BODY: " + c.getBody();
+      
+      System.out.println(s);
     }
-    System.out.println(userCommentIds);
     
-    client.getComments(Long.valueOf(87327973L));
-    
-    //PagedList<Comment> comments = 
-      //client.getComments(userCommentIds.toArray(new Long[0]));
+    System.out.println("***********");  
+    StackExchangeApiQueryFactory queryFactory = StackExchangeApiQueryFactory.newInstance(keyValue, accessToken, StackExchangeSite.STACK_OVERFLOW);
+    CommentApiQuery commentApiQuery = queryFactory.newCommentApiQuery().withFilter("withbody");
+    PagedList<Comment> comments = commentApiQuery.listMyComments();    
+    for (Comment c : comments) { 
+      String s = "";
+      s += "ID: " + c.getCommentId(); 
+      s += " ";
+      s += "OWNER: " + c.getOwner();
+      s += " ";
+      s += "BODY: " + c.getBody();
+      
+      System.out.println(s);
+    }
     
     System.out.println("All done.");
   }
